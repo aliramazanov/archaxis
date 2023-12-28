78db73fe6214b9fbf8e202c7bfb1a70d57217f13
Ali Ramazanov <aliasifzade@gmail.com>
2023-12-28 17:01:00 +0400

    add: "testimonials" component

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -1,5 +1,11 @@
+"use client";
+import React, { useState, useEffect, useRef, useCallback } from "react";
+import Image from "next/image";
+import { motion } from "framer-motion";
+import { BiChevronLeft, BiChevronRight } from "react-icons/bi";
+import { Swiper, SwiperSlide } from "swiper/react";
 import { TestimonialsProps } from "@/types/Types";
-import React, { useState, useEffect } from "react";
+import "swiper/css";
 
 const Testimonials: React.FC<TestimonialsProps> = ({ className }) => {
   return (
