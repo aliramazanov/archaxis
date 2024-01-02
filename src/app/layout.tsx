da05edabb8be88e3b033fe8ca883abb1ae5e14a8
Ali Ramazanov <aliasifzade@gmail.com>
2024-01-02 18:21:27 +0400

    add: favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "archaxis.",
   description: "Architecture Services",
+  icons: {
+    icon: "/icon.png",
+  },
 };
 
 export default function RootLayout({
