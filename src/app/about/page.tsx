d265c6e34264e69f8994c126dc33a3d97407c118
Ali Ramazanov <aliasifzade@gmail.com>
2024-01-05 01:11:23 +0400

    add: about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,14 @@
+import Hero from "@/components/Hero";
+import React from "react";
+
+const page = () => {
+  return (
+    <section>
+      <div className="">
+        <span className="">About</span>
+      </div>
+    </section>
+  );
+};
+
+export default page;
